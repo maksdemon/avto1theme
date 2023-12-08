d7a238f671b56f5e6f3b02b23719078c9c4ee117
mak <worklab@work>
2023-12-08 14:02:01 +0300

    login

---
+++ authphp.php
@@ -0,0 +1,128 @@
+<?php
+// Начать сессию
+
+session_start();
+
+// Установить значение переменной сессии
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["fname"]) && isset($_POST["pas"])) {
+        $fname = $_POST["fname"];
+        $password = $_POST["pas"];
+
+        // Подключение к базе данных
+        $pdo = new mysqli('62.109.2.72', 'avtoparser', '7xXD2rN9i', 'avto1');
+
+        // Проверка подключения
+        if ($pdo->connect_error) {
+            die("Ошибка подключения: " . $pdo->connect_error);
+        }
+
+        // Подготовленный запрос для получения пароля пользователя
+        $stmt = $pdo->prepare("SELECT password FROM user WHERE name = ?");
+        if ($stmt) {
+            $stmt->bind_param("s", $fname);
+            $stmt->execute();
+            $stmt->store_result(); // Для получения количества строк
+
+            if ($stmt->num_rows == 1) {
+                $stmt->bind_result($hashed_password);
+                $stmt->fetch();
+
+                // Вывод данных для отладки
+                echo "Имя пользователя: " . $fname . "<br>";
+                echo "Пароль: " . $password . "<br>";
+                echo "Хеш пароля из базы данных: " . $hashed_password . "<br>";
+
+                // Проверка пароля
+                if (password_verify($password, $hashed_password)) {
+
+                    $_SESSION['username'] = $fname;
+                    if (isset($_SESSION['username'])) {
+                        $username = $_SESSION['username'];
+                        echo "Имя пользователя: $username";
+                    } else {
+                        echo "Сессия не содержит имени пользователя";
+                    }
+
+
+                    echo "Вы успешно авторизованы!";
+                    header("Location: /");
+                    // Редирект на другую страницу или выполнение других действий после успешной авторизации
+                } else {
+                    echo "Неправильное имя пользователя или пароль!";
+
+                }
+            } else {
+                echo "Неправильное имя пользователя или пароль!";
+            }
+        } else {
+            echo "Ошибка при подготовке запроса: " . $pdo->error;
+        }
+
+        // Закрытие подключения
+        $pdo->close();
+    } else {
+        echo "Ошибка: Поля не заполнены!";
+    }
+} else {
+    echo "Ошибка: данные не были отправлены методом POST.";
+}
+
+/*
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["fname"]) && isset($_POST["pas"])) {
+        $fname = $_POST["fname"];
+        $password = $_POST["pas"];
+
+        // Подключение к базе данных
+        $pdo = new mysqli('62.109.2.72', 'avtoparser', '7xXD2rN9i', 'avto1');
+
+        // Проверка подключения
+        if ($pdo->connect_error) {
+            die("Ошибка подключения: " . $pdo->connect_error);
+        }
+
+        // Подготовленный запрос для получения пароля пользователя
+        $stmt = $pdo->prepare("SELECT password FROM user WHERE name = ?");
+        if ($stmt) {
+            $stmt->bind_param("s", $fname);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            if ($result->num_rows == 1) {
+                $row = $result->fetch_assoc();
+                $hashed_password = $row['password'];
+
+
+                // Вывод данных для отладки
+                echo "Имя пользователя: " . $fname . "<br>";
+                echo "Пароль: " . $password . "<br>";
+                echo "Хеш пароля из базы данных: " . $hashed_password . "<br>";
+
+
+                // Проверка пароля
+                if (password_verify($password, $hashed_password)) {
+                    $_SESSION['username'] = $fname;
+                    echo "Вы успешно авторизованы!";
+                    // Редирект на другую страницу или выполнение других действий после успешной авторизации
+                } else {
+                    echo "Неправильное имя пользователя или пароль!";
+                }
+            } else {
+                echo "Неправильное имя пользователя или пароль!";
+            }
+        } else {
+            echo "Ошибка при подготовке запроса: " . $pdo->error;
+        }
+
+        // Закрытие подключения
+        $pdo->close();
+    } else {
+        echo "Ошибка: Поля не заполнены!";
+    }
+} else {
+    echo "Ошибка: данные не были отправлены методом POST.";
+}
+
+*/
--- login.php
+++ login.php
@@ -41,15 +41,16 @@
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                     </div>
-                                    <form class="user">
+                                    <form class="user" method="POST" action="authphp.php">
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
+                                            <input type="name" class="form-control form-control-user"
+                                                   id="nameInput" name="fname"
+                                                   placeholder="Имя ">
                                         </div>
+
                                         <div class="form-group">
                                             <input type="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password">
+                                                id="exampleInputPassword" placeholder="Password" name="pas">
                                         </div>
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
@@ -58,16 +59,9 @@
                                                     Me</label>
                                             </div>
                                         </div>
-                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                            Login
-                                        </a>
+                                        <input  class="btn btn-primary btn-user btn-block" type="submit" value="Авторизация">
+
                                         <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
                                     </form>
                                     <hr>
                                     <div class="text-center">
