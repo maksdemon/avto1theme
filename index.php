c33be4c7611a1ceb27b13c42577ef74ff034e54e
mak <worklab@work>
2023-12-08 14:23:56 +0300

    login2

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+    <?php
+        session_start();
+
+        ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -330,7 +334,20 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">
+
+
+    <?php
+
+        if(isset($_SESSION['username'])) {
+            echo $_SESSION['username'];
+        } else {
+            echo 'Нет сессии';
+        }
+    ?>
+
+
+                                </span>
                                 <img class="img-profile rounded-circle"
                                     src="img/undraw_profile.svg">
                             </a>
