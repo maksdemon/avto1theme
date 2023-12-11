be19733ed5ef896ff4394ce88512dc60379c3f53
makhome <qwerty@ya.ru>
2023-12-11 23:04:40 +0300

    граф тест дубль

--- chart.php
+++ chart.php
@@ -7,7 +7,6 @@ echo "<pre>";
 //var_dump($rowsStartDate );
 echo "</pre>";
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,7 +17,7 @@ echo "</pre>";
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
     <title>SB Admin 2 - Charts</title>
 
     <!-- Custom fonts for this template-->
@@ -382,9 +381,9 @@ echo "</pre>";
 
                         <!-- Area Chart -->
                         <div class="card shadow mb-4">
-                            
-                                <canvas id="mylineChart"></canvas>
-
+                            <div class="card-header py-3">
+                                <h6 class="m-0 font-weight-bold text-primary">Area Chart</h6>
+                            </div>
                             <div class="card-body">
                                 <div class="chart-area">
                                     <canvas id="myAreaChart"></canvas>
@@ -498,100 +497,5 @@ echo "</pre>";
 <script src="js/demo/chart-bar-demo.js"></script>
 
 </body>
-
-</html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<head>
-    <title>График</title>
-    <!-- Подключаем библиотеку Chart.js -->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-</head>
-<body>
-<div class="chart-pie pt-4 pb-2">
-    <canvas id="mylineChart"></canvas>
-</div>
-
-<script>
-    // Ваши данные для линейного графика (пример данных)
-    var data = {
-        labels: ["Янв", "Фев", "Март", "Апр", "Май", "Июн"],
-        datasets: [
-            {
-                label: 'Продажи',
-                data: [120, 150, 180, 170, 200, 190], // Пример данных для первой линии
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)',
-                borderWidth: 2
-            },
-            {
-                label: 'Прибыль',
-                data: [50, 80, 60, 70, 90, 85], // Пример данных для второй линии
-                backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                borderColor: 'rgba(54, 162, 235, 1)',
-                borderWidth: 2
-            }
-        ]
-    };
-
-    // Опции для линейного графика
-    var options = {
-        responsive: true,
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    };
-
-    // Получаем элемент canvas, к которому привязан график
-    var ctx = document.getElementById("mylineChart").getContext("2d");
-
-    // Создаем линейный график
-    var myLineChart = new Chart(ctx, {
-        type: 'line',
-        data: data,
-        options: options
-    });
-</script>
-
-
-</body>
-</html>
-
-
-<!-- Page level plugins -->
-<script src="vendor/chart.js/Chart.min.js"></script>
-
 
 </html>
