ef2c48980c6082bab7745abdecf731e8c0ed18a7
makhome <qwerty@ya.ru>
2023-12-11 10:07:27 +0300

    chart1 тест рабочий

--- chart.php
+++ chart.php
@@ -2,19 +2,6 @@
 
 require('config/config.php');
 
-$sqlStartDate ="
-SELECT 
-    DATE_FORMAT(MIN(date), '%Y-%m-%d') AS start_of_week,
-    MAX(price) AS max_price,
-    MIN(price) AS min_price
-FROM avto1
-WHERE name = 'BOSCH 3397007620'
-GROUP BY YEAR(date), WEEK(date)
-ORDER BY start_of_week";
-
-$resultStartDate = mysqli_query($mysqli, $sqlStartDate);
-$rowsStartDate = mysqli_fetch_all($resultStartDate, MYSQLI_ASSOC);
-$columnNames = array_keys($rowsStartDate[0]);
 
 echo "<pre>";
 //var_dump($rowsStartDate );
@@ -22,37 +9,64 @@ echo "</pre>";
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
-<link href="css/style.css" rel="stylesheet">
 
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>SB Admin 2 - Tables</title>
-    <!-- Custom fonts for this template -->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-    <!-- Custom styles for this template -->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
-    <!-- Custom styles for this page -->
-    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <title>График</title>
+    <!-- Подключаем библиотеку Chart.js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
+<body>
+<div class="chart-pie pt-4 pb-2">
+    <canvas id="mylineChart"></canvas>
+</div>
+
+<script>
+    // Ваши данные для линейного графика (пример данных)
+    var data = {
+        labels: ["Янв", "Фев", "Март", "Апр", "Май", "Июн"],
+        datasets: [
+            {
+                label: 'Продажи',
+                data: [120, 150, 180, 170, 200, 190], // Пример данных для первой линии
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 2
+            },
+            {
+                label: 'Прибыль',
+                data: [50, 80, 60, 70, 90, 85], // Пример данных для второй линии
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 2
+            }
+        ]
+    };
+
+    // Опции для линейного графика
+    var options = {
+        responsive: true,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    };
+
+    // Получаем элемент canvas, к которому привязан график
+    var ctx = document.getElementById("mylineChart").getContext("2d");
+
+    // Создаем линейный график
+    var myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: options
+    });
+</script>
 
 
-
-
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-<!-- Core plugin JavaScript-->
-<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
+</body>
+</html>
 
 
 <!-- Page level plugins -->
